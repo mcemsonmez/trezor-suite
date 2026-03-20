f335e5266129acd0b5384785f81adae7150e6239
Peter Sanderson <dierk.deidel@protonmail.com>
2026-03-20 10:25:47 +0100

    fix:: redux stuff

--- suite-native/atoms/redux.d.ts
+++ suite-native/atoms/redux.d.ts
@@ -1,7 +1,10 @@
 import { AsyncThunkAction, ThunkAction } from '@reduxjs/toolkit';
+import type { Action, AnyAction } from 'redux';
 
 declare module 'redux' {
     export interface Dispatch<A extends Action = AnyAction> {
+        <TAction extends A>(action: TAction): TAction;
+
         <TThunk extends AsyncThunkAction<any, any, any>>(thunk: TThunk): ReturnType<TThunk>;
 
         <ReturnType = any, State = any, ExtraThunkArg = any>(
