3356e30404c3d7e519ddd8c53d09db7bca114150
Mattew Husak <matej.husak@icloud.com>
2026-03-22 21:11:45 +0100

    feat(suite): add IDB migration to rename experimentalFeedback to featureFeedback

--- packages/suite/src/storage/migrations/versions/26.4.0.1.ts
+++ packages/suite/src/storage/migrations/versions/26.4.0.1.ts
@@ -22,4 +22,21 @@ export default createMigration<SuiteDBSchema>('26.4.0.1', async (db, tx) => {
             return device;
         }
     });
+
+    if (!db.objectStoreNames.contains('featureFeedback')) {
+        db.createObjectStore('featureFeedback');
+    }
+
+    // @ts-expect-error experimentalFeedback no longer exists in the schema
+    if (db.objectStoreNames.contains('experimentalFeedback')) {
+        // @ts-expect-error experimentalFeedback no longer exists in the schema
+        const oldData = await tx.objectStore('experimentalFeedback').get('experimentalFeedback');
+
+        if (oldData) {
+            tx.objectStore('featureFeedback').put(oldData, 'featureFeedback');
+        }
+
+        // @ts-expect-error experimentalFeedback no longer exists in the schema
+        db.deleteObjectStore('experimentalFeedback');
+    }
 });
