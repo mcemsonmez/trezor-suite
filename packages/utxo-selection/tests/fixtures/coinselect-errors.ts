eceb940943656f060fbe53e898d9dad26e32d43d
Szymon Lesisz <sz.lesisz@gmail.com>
2026-03-30 16:04:53 +0200

    feat(utxo-selection): add `coinselect` and `compose` modules

---
+++ packages/utxo-selection/tests/fixtures/coinselect-errors.ts
@@ -0,0 +1,14 @@
+export const coinselectFixturesErrors = [
+    {
+        description: 'missing input info',
+        feeRate: 10,
+        inputs: [
+            {
+                value: '102001',
+            },
+        ],
+        outputs: ['100000'],
+        expected: 'Unexpected unreturned result',
+        dustThreshold: 546,
+    },
+];
