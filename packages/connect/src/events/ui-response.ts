3b43f964fae1999f8148d3e3ae76218f4835a915
karliatto <yo@karliatto.com>
2026-03-31 12:49:24 +0200

    refactor(suite): add requestId to ui-receive_thp_pairing_tag

--- packages/connect/src/events/ui-response.ts
+++ packages/connect/src/events/ui-response.ts
@@ -58,6 +58,7 @@ export interface UiResponseThpPairingTag {
         | {
               selectedMethod: ThpPairingMethod | keyof typeof ThpPairingMethod;
           };
+    requestId?: string;
 }
 export interface UiResponseAccount {
     type: typeof UI_RESPONSE.RECEIVE_ACCOUNT;
