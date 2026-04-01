d84344788706d061fb2c20697045440880989503
STew790 <stepan.janda@satoshilabs.com>
2026-04-01 17:16:56 +0200

    fix(suite): show imported labels in send form outputs

--- packages/suite/src/views/wallet/send/Outputs/Address.tsx
+++ packages/suite/src/views/wallet/send/Outputs/Address.tsx
@@ -491,7 +491,9 @@ export const Address = ({ output, outputId, outputsCount }: AddressProps) => {
 
                                     return Promise.resolve(true);
                                 }}
-                            />
+                            >
+                                {label}
+                            </Labeling>
                         </Text>
                     )}
                     {outputsCount > 1 && (
