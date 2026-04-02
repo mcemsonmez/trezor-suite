56f7980eed63ec977609b155d070bd69402d75f5
honzauher <jan.uher@satoshilabs.com>
2026-04-02 12:23:20 +0200

    feat(suite-e2e): rework migration test from legacy to Suite Sync

--- suite/e2e/tests/metadata/legacy-label-to-suiteSync.test.ts
+++ suite/e2e/tests/metadata/legacy-label-to-suiteSync.test.ts
@@ -1,26 +1,18 @@
-import { mnemonic12Fixtures } from '@suite-common/e2e-evolu-client';
-
 import { AccountLabelId } from '../../support/enums/accountLabelId';
 import { expect, test } from '../../support/fixtures';
 import { MetadataProvider } from '../../support/mocks/metadataMock';
 
-const { accountSeed, ownerSecret, ownerId } = mnemonic12Fixtures;
+const localLabel = 'local label';
 
-test.describe('Labeling migration', { tag: ['@webOnly', '@T3W1', '@T3T1'] }, () => {
+test.describe('Labeling migration', { tag: ['@T3W1', '@T3T1'] }, () => {
     test.use({ wipeEvoluRelay: true });
 
-    test.beforeEach(async ({ metadataMock, evoluClient, onboardingPage }) => {
-        await metadataMock.start(MetadataProvider.DROPBOX);
-        await test.step('Seed Evolu relay server', () => {
-            evoluClient.init({ ownerSecret });
-            evoluClient.writeTo('account', accountSeed);
-            evoluClient.seedQuotaManagerData({ ownerId });
-        });
+    test.beforeEach(async ({ onboardingPage }) => {
         await onboardingPage.completeOnboarding({ keepDebugModeEnabled: true });
     });
 
-    test('Migration from Dropbox', async ({ page, walletPage, metadataPage }) => {
-        await test.step('Set up Dropbox labeling', async () => {
+    test('Migration from local file', async ({ page, walletPage, metadataPage, devicePrompt }) => {
+        await test.step('Set up local file labeling', async () => {
             await walletPage.accountLabel({ symbol: 'btc', type: 'normal', atIndex: 0 }).click();
             await expect(
                 walletPage.accountLabel({ symbol: 'btc', type: 'normal', atIndex: 0 }),
@@ -29,23 +21,26 @@ test.describe('Labeling migration', { tag: ['@webOnly', '@T3W1', '@T3T1'] }, () 
             // wait until account page is fully loaded
             await expect(walletPage.fiatAmount).toBeVisible();
             await metadataPage.account.clickEditLabelButton(AccountLabelId.BitcoinDefault1);
-            await metadataPage.passThroughInitMetadata(MetadataProvider.DROPBOX);
+            await metadataPage.passThroughInitMetadata(MetadataProvider.LOCAL);
         });
 
         await test.step('Add legacy account label', async () => {
-            await metadataPage.account.metadataInput.fill('new dropbox label');
+            await metadataPage.account.metadataInput.fill(localLabel);
             await page.keyboard.press('Enter');
             await expect(
                 walletPage.accountLabel({ symbol: 'btc', type: 'normal', atIndex: 0 }),
-            ).toHaveText('new dropbox label');
+            ).toHaveText(localLabel);
             await metadataPage.account.successIconIsVisible(AccountLabelId.BitcoinDefault1);
         });
 
-        await test.step('Switch to Suite Sync labeling and confirm legacy label is retained', async () => {
+        await test.step('Switch to Suite Sync labeling and confirm legacy label is migrated', async () => {
             await metadataPage.enableSuiteSync();
+            await metadataPage.migrateLabelsButton.click();
+            await metadataPage.migrateFromLocalFileButton.click();
+            await devicePrompt.waitForPromptAndConfirm();
             await expect(
                 walletPage.accountLabel({ symbol: 'btc', type: 'normal', atIndex: 0 }),
-            ).toHaveText(accountSeed.label);
+            ).toHaveText(localLabel);
         });
     });
 });
